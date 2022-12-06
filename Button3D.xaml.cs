eff3b95790ad9b75a1d96e5ca694f034f8edb04f
user <user@WIN-722IPQ4TV8B>
2022-12-06 19:01:13 +0400

    Add project files.

---
+++ Button3D.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RND_10
+{
+    /// <summary>
+    /// Interaction logic for Button3D.xaml
+    /// </summary>
+    public partial class Button3D : UserControl
+    {
+        public Button3D()
+        {
+            InitializeComponent();
+        }
+    }
+}
